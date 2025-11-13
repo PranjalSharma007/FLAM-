f380cec317c3d68eab89868e500be68f7d9c91cb
PranjalSharma007 <pranjalsharma.tms@gmail.com>
2025-11-13 14:59:59 +0530

    update

--- queuectl/config.py
+++ queuectl/config.py
@@ -1,1 +1,18 @@
-# config
+# config.py
+class Config:
+    def __init__(self):
+        self.backoff_base = 2
+        self.default_max_retries = 3
+
+_cfg = Config()
+
+def get_config():
+    return _cfg
+
+def config_set(key: str, value: str):
+    if key in ("backoff-base", "backoff_base"):
+        _cfg.backoff_base = int(value)
+    elif key in ("default-max-retries", "max-retries", "default_max_retries"):
+        _cfg.default_max_retries = int(value)
+    else:
+        raise KeyError(f"unknown config key {key}")
